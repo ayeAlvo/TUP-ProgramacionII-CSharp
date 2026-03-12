030c623e608bc9f04f185199b842178a1903f43e
ayeAlvo <ayealvo@gmail.com>
2026-03-12 13:26:34 -0300

    feat(poo-basico): add Persona class exercise

---
+++ 01-poo-basico/persona-ejercicio/Persona.cs
@@ -0,0 +1,5 @@
+public class Persona
+{
+    public string Nombre;
+    public string Apellido;
+}
---
+++ 01-poo-basico/persona-ejercicio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Persona persona = new Persona();
+        persona.Nombre = "Juan";
+        persona.Apellido = "Pérez";
+        Console.WriteLine("Nombre: " + persona.Nombre);
+        Console.WriteLine("Apellido: " + persona.Apellido);
+    }
+}
