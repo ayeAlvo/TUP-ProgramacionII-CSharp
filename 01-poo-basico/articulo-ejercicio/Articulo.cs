68811dddbeca321531a00812fa15d979c6289cdf
ayeAlvo <ayealvo@gmail.com>
2026-03-12 13:57:55 -0300

    feat(poo-basico): add Articulo exercise

---
+++ 01-poo-basico/articulo-ejercicio/Articulo.cs
@@ -0,0 +1,15 @@
+public class Articulo
+{
+    private string Marca;
+    private string Modelo;
+
+    public void SetMarca(string marca)
+    {
+        Marca = marca;
+    }
+
+    public void SetModelo(string modelo)
+    {
+        Modelo = modelo;
+    }
+}
---
+++ 01-poo-basico/articulo-ejercicio/Program.cs
@@ -0,0 +1,10 @@
+﻿var art = new Articulo();
+
+art.SetMarca("Samsung");
+art.SetModelo("Galaxy S22");
+
+// ❌ No se puede hacer esto:
+// Console.WriteLine(art.Marca);
+// Console.WriteLine(art.Modelo);
+
+// Esto genera error porque los atributos son privados.
